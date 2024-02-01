762a8ba9bf2e9eff046d211684ca118c802629a3
Christian Manzaraz <magnusmanz@gmail.com>
2024-02-01 18:15:15 -0300

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Christian Manzaraz on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
